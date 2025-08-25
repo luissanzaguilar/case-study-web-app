29619789c31c1253c165c508154bd01f835925f0
Luis <luissanzaguilar@gmail.com>
2025-08-25 19:12:13 +0200

    Added welcome message depending on the environment

--- src/main/java/lsz/training/web/main/controller/Controller.java
+++ src/main/java/lsz/training/web/main/controller/Controller.java
@@ -1,5 +1,6 @@
 package lsz.training.web.main.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,14 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class Controller {
 
+
+    @Value("${app.welcome.message:Welcome default message.}")
+    private String appMessage;
     private static final String template = "Main application, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/")
     public Main main(@RequestParam(defaultValue = "You can start") String name) {
-        return new Main(counter.incrementAndGet(), String.format(template, name));
+        return new Main(counter.incrementAndGet(), String.format(appMessage + template, name));
     }
 }
