64c67ec1dec4d688a899a9c6b45bac7ec4618b29
Luis <luissanzaguilar@gmail.com>
2025-08-26 01:36:00 +0200

    Terraform base files

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,17 +18,48 @@ data "aws_ami" "amazon-linux" {
   }
 }
 
-resource "aws_iam_instance_profile" "role_terraform" {
+resource "aws_iam_role" "role_terraform" {
   name = "role-terraform"
-  role = "role-terraform"
-  path = "/"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "role-terraform"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "admin_access" {
+  role       = aws_iam_role.role_terraform.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  depends_on = [aws_iam_role.role_terraform]
+}
+
+resource "aws_iam_instance_profile" "profile_terraform" {
+  name = "profile-terraform"
+  role = aws_iam_role.role_terraform.name
+
+  depends_on = [
+    aws_iam_role.role_terraform
+  ]
+
 }
 
 resource "aws_instance" "app_terraform" {
   ami           = data.aws_ami.amazon-linux.id
   instance_type = "m7i-flex.large"
   key_name = "formacion"
-  iam_instance_profile = "role-terraform"
+  iam_instance_profile = aws_iam_instance_profile.profile_terraform.name
   root_block_device {
     volume_size = 20
   }
@@ -36,4 +67,8 @@ resource "aws_instance" "app_terraform" {
   tags = {
     Name = "case-study-2-terraform"
   }
+
+  depends_on = [
+    aws_iam_instance_profile.profile_terraform
+  ]
 }
