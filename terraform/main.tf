6124b98ba1a62c50669bfbab78dff3169b707c9c
Luis <luissanzaguilar@gmail.com>
2025-08-26 00:41:32 +0200

    Terraform base files

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+# Instance ID: i-02c7ee74fa86fffa4
+
+# Aquí definiremos los recursos después de importarlos
+resource "aws_instance" "existing_ec2" {
+  # Los atributos se llenarán después de la importación
+}
+
+resource "aws_iam_role" "existing_role" {
+  # Los atributos se llenarán después de la importación
+}
+
+resource "aws_iam_instance_profile" "existing_profile" {
+  # Los atributos se llenarán después de la importación
+}
