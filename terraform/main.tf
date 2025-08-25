fe143a4a4a874f9d4cc88128f6522712f4692ccc
Luis <luissanzaguilar@gmail.com>
2025-08-26 01:12:21 +0200

    Terraform base files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,32 @@
-# Instance ID: i-02c7ee74fa86fffa4
 
-# Aquí definiremos los recursos después de importarlos
-resource "aws_instance" "existing_ec2" {
-  # Los atributos se llenarán después de la importación
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.8.20250818.0-kernel-6.1-x86_64"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_iam_role" "existing_role" {
-  # Los atributos se llenarán después de la importación
+resource "aws_iam_instance_profile" "role_terraform" {
+  id = "role-terraform"
+  name = "role-terraform"
+  role = "role-terraform"
+  path = "/"
 }
 
-resource "aws_iam_instance_profile" "existing_profile" {
-  # Los atributos se llenarán después de la importación
+resource "aws_instance" "app_terraform" {
+  ami           = data.aws_ami.amazon-linux.id
+  instance_type = "m7i-flex.large"
+  key_name = "formacion"
+  iam_instance_profile = "role-terraform"
+  root_block_device {
+    volume_size = 20
+  }
+
+  tags = {
+    Name = "case-study-2-terraform"
+  }
 }
