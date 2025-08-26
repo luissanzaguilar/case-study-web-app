17bb20fac3d840186cf897469455bd1256d5aadb
Luis <luissanzaguilar@gmail.com>
2025-08-26 02:06:54 +0200

    Terraform with user data install

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,6 +60,39 @@ resource "aws_instance" "app_terraform" {
   instance_type = "m7i-flex.large"
   key_name = "formacion"
   iam_instance_profile = aws_iam_instance_profile.profile_terraform.name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              set -e
+
+              # Update system
+              sudo yum update -y
+
+              # Install Docker
+              sudo yum install -y docker
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker ec2-user
+              sudo systemctl daemon-reload
+              sudo systemctl restart docker
+
+              # Install Minikube
+              curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+              sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+              # Install kubectl
+              sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
+              sudo chmod +x ./kubectl
+              sudo mv ./kubectl /usr/local/bin/kubectl
+
+              # Install Terraform
+              sudo yum install -y yum-utils shadow-utils
+              sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+              sudo yum install -y terraform
+
+              echo "=== Instalación completada ==="
+              EOF
+
   root_block_device {
     volume_size = 20
   }
