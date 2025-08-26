2a43102faa825588fe43d56f433418b42b2296eb
Luis <luissanzaguilar@gmail.com>
2025-08-26 16:04:07 +0200

    Workflows for terraform deployments

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "my-terraform-state-bucket"
-    key    = "terraform.tfstate"
+    key    = "terraform/terraform.tfstate"
     region = var.aws_region
+    encrypt = true
   }
 }
